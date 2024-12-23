a8053344dcaa67f0f35d7741faedeb4c29654abb
Nousad <nousadaliinps@gmail.com>
2024-12-24 00:33:50 +0530

    about component Done

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,26 +1,53 @@
-import React from 'react'
-import About_ele from './About_ele'
+import React from "react";
+import About_ele from "./About_ele";
 
 const About = () => {
   return (
-    <div className=' w-dvw h-[80dvh] -mt-12 mb-20'>
-    
-     <div className='relative bg-[#17414F] w-10/12 h-[28rem] rounded-[5rem] m-auto '>
-         <div className='absolute -mt-12 inset-0 '>
-            <div className='flex justify-center items-center'>
-               <About_ele img='/images/about-1.png' img_alt='about-image1' heading='Clinically Studied' subheading='All products that we offer have undergone lab and safety tests'/>
-               <About_ele img='/images/about-2.png' img_alt='about-image2' heading='Vegetarian Friendly' subheading='We have a wide selection of vegetarian products to meet your needs'/>
-               <About_ele img='/images/about-3.png' img_alt='about-image3' heading='Made In India' subheading='Shop local and explore health products made right here in India'/>
-            </div>
-            <div className='flex justify-center items-center'>
-               <About_ele img='/images/about-4.png' img_alt='about-image4' heading='Free Shipping' subheading='We deliver to your door with no shipping costs on your orders'/>
-               <About_ele img='/images/about-5.png' img_alt='about-image5' heading='No Risk' subheading='We ensure that all products are safe and within their use-by date'/>
-               <About_ele img='/images/about-6.png' img_alt='about-image6' heading='GMO Free' subheading='Natural, no modified products and derivatives for those who need it'/>
-            </div>
-         </div>
+    <div className="w-full lg:max-h-96 sm:h-[90dvh] mb-20">
+      <div className="relative bg-[#17414F] lg:max-w-[77rem] sm:w-10/12 w-11/12 lg:max-h-[26rem] sm:h-dvh h-[190dvh] rounded-3xl m-auto p-4 sm:p-10">
+        <div className="absolute grid lg:grid-cols-3 sm:grid-cols-2 gap-4 sm:gap-2 text-center md:-left-2 sm:-mt-[90px] -mt-16 -left-2 ">
+          <About_ele
+            img="/images/about-1.png"
+            img_alt="clinically-studied-icon"
+            heading="Clinically Studied"
+            subheading="All products that we offer have undergone lab and safety tests"
+          />
+          <About_ele
+            img="/images/about-2.png"
+            img_alt="vegetarian-friendly-icon"
+            heading="Vegetarian Friendly"
+            subheading="We have a wide selection of vegetarian products to meet your needs"
+          />
+          <About_ele
+            img="/images/about-3.png"
+            img_alt="made-in-india-icon"
+            heading="Made In India"
+            subheading="Shop local and explore health products made right here in India"
+          />
+        {/* </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-4 sm:gap-6 text-center mt-6"> */}
+          <About_ele
+            img="/images/about-4.png"
+            img_alt="free-shipping-icon"
+            heading="Free Shipping"
+            subheading="We deliver to your door with no shipping costs on your orders"
+          />
+          <About_ele
+            img="/images/about-5.png"
+            img_alt="no-risk-icon"
+            heading="No Risk"
+            subheading="We ensure that all products are safe and within their use-by date"
+          />
+          <About_ele
+            img="/images/about-6.png"
+            img_alt="gmo-free-icon"
+            heading="GMO Free"
+            subheading="Natural, no modified products and derivatives for those who need it"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- app/components/About_ele.tsx
+++ app/components/About_ele.tsx
@@ -1,24 +1,30 @@
-import Image, { StaticImageData } from 'next/image'
-import React from 'react'
+import Image, { StaticImageData } from "next/image";
+import React from "react";
 
-type AboutProps={
-   img : StaticImageData| string,
-   img_alt : string,
-   heading : string,
-   subheading: string
-}
+type AboutProps = {
+  img: StaticImageData | string;
+  img_alt: string;
+  heading: string;
+  subheading: string;
+};
 
-
-const About_ele : React.FC<AboutProps>= ({img,img_alt, heading, subheading}) => {
+const About_ele: React.FC<AboutProps> = ({ img, img_alt, heading, subheading }) => {
   return (
-   <div className=''>
-      <div className='text-white font-jost text-center w-full px-20 py-2 m-2'>
-         <Image className='m-auto' src={img} alt={img_alt} width={80} height={80}/>
-         <h1 className='font-medium text-2xl my-3'>{heading}</h1>
-         <p className='font-light text-base'>{subheading}</p> 
+    <div className="w-full px-4 py-4 m-2 sm:px-10">
+      <div className="text-white font-jost text-center">
+        <Image
+          className="m-auto"
+          src={img}
+          alt={img_alt}
+          width={60}
+          height={60}
+          sizes="(max-width: 768px) 60px, (max-width: 1024px) 80px, 80px" 
+        />
+        <h1 className="font-medium text-xl md:text-2xl my-3">{heading}</h1>
+        <p className="font-light text-sm md:text-base sm:p-1 px-3">{subheading}</p>
       </div>
-   </div>
-  )
-}
+    </div>
+  );
+};
 
-export default About_ele
+export default About_ele;
