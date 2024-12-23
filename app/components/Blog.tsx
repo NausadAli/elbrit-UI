f5f259b40ef33b37f907c3a338bacd5460567cdb
Nousad <nousadaliinps@gmail.com>
2024-12-23 22:14:42 +0530

    Make Blog Component Responsive

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -3,34 +3,77 @@ import Blog_ele from './Blog_ele'
 
 const Blog = () => {
   return (
-    <div>
-
-      <div className=' text-center my-2'>
-         <h3 className='font-jost font-medium text-sm uppercase my-1'>
-            Our Blog 
-         </h3>
-         <h1 className='font-jost font-semibold text-4xl'>
-            Latest News
-         </h1>
+    <div className='my-4'>
+      {/* Header Section */}
+      <div className="text-center my-2">
+        <h3 className="text-[#003569] font-jost font-medium text-sm uppercase my-1">
+          Our Blog
+        </h3>
+        <h1 className="text-[#17414F] font-jost font-semibold text-4xl my-3">Latest News</h1>
       </div>
 
-       <div className="flex w-dvw justify-center gap-2 my-3">
-        <div>
-          <Blog_ele date ="21 Oct" text="The Covid Epidemic 2023 is back" img="/images/meds-1.png" img_alt="news image"/>
-          <Blog_ele date ="02 Jan" text="The Covid Epidemic 2022 is back" img="/images/meds-2.png" img_alt="news image" heightClass="h-[24dvh]"/>
-        </div> 
-        <div>
-          <Blog_ele date ="20 Jan" text="The Covid Epidemic is back again" img="/images/meds-2.png" img_alt="news image" heightClass="h-[24dvh]"/>
-          <Blog_ele date ="20 Mar" text="The Covid Epidemic 2022 is back" img="/images/meds-1.png" img_alt="news image"/>
-        </div> 
-        <div>
-          <Blog_ele date ="20 Apr" text="The Covid Epidemic 2023 is back" img="/images/meds-1.png" img_alt="news image"/>
-          <Blog_ele date ="20 Apr" text="The Covid Epidemic 2024 is back" img="/images/meds-2.png" img_alt="news image" heightClass="h-[24dvh]"/>
-        </div> 
-        <div>
-          <Blog_ele date ="20 Apr" text="The Covid Epidemic 2023 is back" img="/images/meds-2.png" img_alt="news image" heightClass="h-[24dvh]"/>
-          <Blog_ele date ="20 Apr" text="The Covid Epidemic 2023 is back" img="/images/meds-1.png" img_alt="news image"/>
-        </div>         
+      {/* Blog Section */}
+      <div className="flex flex-col items-center gap-4 md:flex-row md:justify-center md:gap-2 w-dvw my-4">
+        <div className="flex flex-col items-center gap-2">
+          <Blog_ele
+            date="21 Oct"
+            text="The Covid Epidemic 2023 is back"
+            img="/images/meds-1.png"
+            img_alt="news image"
+          />
+          <Blog_ele
+            date="02 Jan"
+            text="The Covid Epidemic 2022 is back"
+            img="/images/meds-2.png"
+            img_alt="news image"
+            heightClass="h-[24dvh]"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <Blog_ele
+            date="20 Jan"
+            text="The Covid Epidemic is back again"
+            img="/images/meds-2.png"
+            img_alt="news image"
+            heightClass="h-[24dvh]"
+          />
+          <Blog_ele
+            date="20 Mar"
+            text="The Covid Epidemic 2022 is back"
+            img="/images/meds-1.png"
+            img_alt="news image"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <Blog_ele
+            date="20 Apr"
+            text="The Covid Epidemic 2023 is back"
+            img="/images/meds-1.png"
+            img_alt="news image"
+          />
+          <Blog_ele
+            date="20 Apr"
+            text="The Covid Epidemic 2024 is back"
+            img="/images/meds-2.png"
+            img_alt="news image"
+            heightClass="h-[24dvh]"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <Blog_ele
+            date="20 Apr"
+            text="The Covid Epidemic 2023 is back"
+            img="/images/meds-2.png"
+            img_alt="news image"
+            heightClass="h-[24dvh]"
+          />
+          <Blog_ele
+            date="20 Apr"
+            text="The Covid Epidemic 2023 is back"
+            img="/images/meds-1.png"
+            img_alt="news image"
+          />
+        </div>
       </div>
     </div>
   )
--- app/components/Blog_ele.tsx
+++ app/components/Blog_ele.tsx
@@ -12,12 +12,12 @@ type BlogProps = {
 const Blog_ele: React.FC<BlogProps>  = ({date, text, img, img_alt, heightClass}) => {
   return (
     <div className='relative text-white py-2'>
-      <div className='absolute font-jost px-4 py-1 pl-4 w-20 text-sm font-normal z-20 bg-[#003569] top-5 ml-1.5 rounded-r-full'>
+      <div className='absolute font-jost px-4 py-1 pl-4 w-22 md:text-sm font-normal z-20 bg-[#003569] top-5 ml-1.5 rounded-r-full'>
          {date}
       </div>
-      <div className={`relative w-[21dvw] ${heightClass || 'h-[54dvh]'} overflow-hidden rounded-xl ml-4`}>
-         <div className='absolute w-full z-10 top-12 flex justify-center px-5'>
-            <div className='font-jost text-lg font-medium bg-orange-500/40
+      <div className={`relative w-[85dvw] md:w-[21dvw] ${heightClass || 'h-[54dvh]'} overflow-hidden rounded-xl ml-4`}>
+         <div className='absolute w-full z-10 top-16 md:top-12 flex justify-center px-5'>
+            <div className='font-jost text-2xl font-bold md:text-lg md:font-medium bg-orange-500/20
              rounded-lg pl-1'>
                {text}
             </div>
