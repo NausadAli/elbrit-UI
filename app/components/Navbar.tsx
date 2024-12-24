d0445b77236f22876429ddb51b8d6361eb3ee629
Nousad <nousadaliinps@gmail.com>
2024-12-24 12:22:22 +0530

    Navbar Added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className='h-16 w-dvw bg-white border-black'>
-      
-    </div>
+    
+
+<nav className="">
+  <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+  <div className="relative w-40 h-8 overflow-hidden">
+            <Image
+              src="/images/mainLogo.jpg" 
+              alt="Centered Image"
+              layout="fill" 
+              objectFit="cover" 
+              objectPosition="center" 
+              />
+          </div>
+  <button className=" uppercase bg-[#17414F] py-2 px-6 lg:py-3 lg:pl-8 lg:pr-16 text-base font-semibold text-white rounded-[2rem]">
+      Call to Actions
+  </button>
+  </div>
+</nav>
+
   )
 }
 
