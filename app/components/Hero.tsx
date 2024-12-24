f46e43dee03165fe7bee3d24c4cec78019f42cdc
Nousad <nousadaliinps@gmail.com>
2024-12-24 12:22:41 +0530

    Bux Fixed

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,54 +1,69 @@
-
 import React from "react";
 import Image from "next/image";
 import Hero_sub from "./Hero_sub";
 
 export default function Hero() {
    return (
-      <section className="flex justify-center mt-2 h-[115dvh] w-full -mb-12">
-         <div className="bg-[#E2F5EB] h-[145dvh] w-[98dvw]">
+
+       <div className="flex justify-center m-2 lg:h-[120dvh] md:h-[185dvh] sm:h-[180dvh] h-[170dvh] -mb-12">
+         <div className="bg-[#E2F5EB] h-[185dvh] w-full">
             <div>
-               <h1 className="font-jost text-center font-bold text-[9dvw] text-[#003569] mt-9">
+               <h1 className="font-jost text-[#003569] text-center font-extrabold 2xl:text-[900%] xl:text-[780%] lg:text-[650%] md:text-[485%] sm:text-[385%] text-[10dvw]  mt-9">
                   Essential Vitamins
                </h1>
             </div>
 
-            <div className="flex justify-center font-jost gap-8">
-               <div className="w-3/12 my-auto">
-                  <h2 className="my-2 text-[#727272] font-[300px] text-2xl">
+            <div className="flex flex-col lg:flex-row justify-center items-center font-jost gap-8">
+              
+               <div className="max-w-80 w-10/12 lg:w-3/12 my-2 text-center lg:text-left">
+                  <h2 className="my-2 text-[#727272] font-light text-lg sm:text-xl">
                      Online Medical Supplies
                   </h2>
-                  <h1 className="my-2 text-[#17414F] font-medium text-3xl">
+                  <h1 className="my-2 text-[#17414F] font-medium text-xl sm:text-2xl">
                      Get Your Vitamins & Minerals
                   </h1>
-                  <button className="my-3 uppercase bg-[#17414F] py-3 pl-8 pr-16 text-sm font-semibold text-white rounded-[2rem]">
+                  <button className="my-3 uppercase bg-[#17414F] py-2 px-6 lg:py-3 lg:pl-8 lg:pr-16 text-sm font-semibold text-white rounded-[2rem]">
                      Explore
                   </button>
                </div>
 
-               <div className="relative bg-[#FFE9B5] w-72 h-80 rounded-tl-[40%] rounded-br-[40%] mr-4">
+
+               <div className="relative bg-[#FFE9B5] w-60 h-60 sm:w-72 sm:h-80 rounded-tl-[40%] rounded-br-[40%] flex-shrink-0 ml-10">
                   <Image
                      src="/images/hero-image.png"
                      alt="Illustration of essential vitamins"
                      fill={true}
                      objectFit="cover"
                      priority={true}
-                     className="absolute scale-125 -mt-14 -ml-7"
+                     className="absolute scale-135 sm:scale-125 -mt-[2.5rem] md:-mt-[1.5rem] lg:-mt-[6rem]"
                   />
                </div>
 
-               <div className="w-[27%] my-auto ml-10 -mr-14">
-                  <Hero_sub img="/images/hero-1.png" img_alt="vitamin icon" heading="Vitamins" subheading="Increased Vitamins and <br/> minerals in your diet"/>
-                  <Hero_sub img="/images/hero-2.png" img_alt="weight loss icon" heading="Weight Loss" subheading="Weight Loss <br/> Find scientifically proven solutions"/>
-                  <Hero_sub img="/images/hero-3.png" img_alt="food icon" heading="Functional Foods" subheading="Functional Foods <br/> From protein powers to baby formula"/>
+
+               <div className="md:w-80 lg:w-[27%] lg:ml-10 pl-10">
+                  <Hero_sub
+                     img="/images/hero-1.png"
+                     img_alt="vitamin-icon"
+                     heading="Vitamins"
+                     subheading="Increased Vitamins and <br/> minerals in your diet"
+                  />
+                  <Hero_sub
+                     img="/images/hero-2.png"
+                     img_alt="weight-loss-icon"
+                     heading="Weight Loss"
+                     subheading="Weight Loss <br/> Find scientifically proven solutions"
+                  />
+                  <Hero_sub
+                     img="/images/hero-3.png"
+                     img_alt="food-icon"
+                     heading="Functional Foods"
+                     subheading="Functional Foods <br/> From protein powers to baby formula"
+                  />
                </div>
+
             </div>
 
-            <div>
-               <div></div>
-               <div></div>
-            </div>
          </div>
-      </section>
+      </div>
    );
 }
--- app/components/Hero_sub.tsx
+++ app/components/Hero_sub.tsx
@@ -12,12 +12,12 @@ type HeroProps = {
 
 const Hero_sub: React.FC<HeroProps>= ({img, img_alt, heading, subheading}) => {
   return (
-    <section className='flex my-4 justify-center items-center gap-3 pl-4'>
+    <section className='flex my-4 justify-center items-center gap-5 pl-4'>
         
          <Image src={img} alt={img_alt} width={70} height={70} />       
-         <div className='w-full my-1'>
-            <h1 className='text-[#17414F] font-jost text-2xl font-bold'>{heading}</h1>
-            <p className='text-[#727272] font-jost font-light text-base'dangerouslySetInnerHTML={{ __html: subheading }} />
+         <div className='w-[99%] my-2'>
+            <h1 className='text-[#17414F] font-jost text-2xl font-bold pt-2'>{heading}</h1>
+            <p className='text-[#727272] font-jost font-normal px-2 text-base'dangerouslySetInnerHTML={{ __html: subheading }} />
          </div>
     </section>
   )
