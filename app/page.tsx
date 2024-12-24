cd4dd5783fa1b7285a33683911c01c18d040d437
Nousad <nousadaliinps@gmail.com>
2024-12-24 16:20:48 +0530

    improved performance in Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 // import Image from "next/image";
+
+import dynamic from "next/dynamic";
 import Navbar from "./components/Navbar";
+// const HeroContent = dynamic(()=> import('./components/Hero'), {
+//   ssr: false
+// })
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Blog from "./components/Blog";
 import Footer from "./components/Footer";
 import Essentials from "./components/Essentials";
-// const NoSSR = dynamic(() => import('../components/no-ssr'), { ssr: false })
 
 export default function Home() {
   return (
